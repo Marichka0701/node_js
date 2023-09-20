f99d5bb63ad7aa56fa760bfb30af10ca277fb6ff
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-09-20 11:03:27 +0300

    homework 1 added

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
-// const {sayHello} = require('./sayHello');
-//
-// sayHello();
+const fs = require('fs/promises');
+const path = require('path');
+const {readdir} = require('node:fs/promises');
 
-// const os = require('os');
-// console.log(os.arch());
+const filePath = path.join(__dirname, 'folder');
+
+const createFiles= async () => {
+    try {
+        const directory = await fs.mkdir(filePath,(err) => {
+            if (err) throw new Error(err.message);
+        });
+        for (let i = 0; i < 5; i++) {
+            await fs.mkdir(path.join(filePath, `subfolder${i}`), (err) => {
+                if (err) throw new Error(err.message);
+            });
+            await fs.writeFile(path.join(filePath, `subfile${i}`), `text in file${i}`, (err) => {
+                if (err) throw new Error(err.message);
+            });
+        }
+
+        console.log(`Children in ${filePath} were successful created :)`);
+
+        childIsDirectoryOrFile();
+    } catch (e) {
+        throw new Error(e.message);
+    }
+}
+
+const childIsDirectoryOrFile = async () => {
+    try {
+        const directoryInfo = await fs.readdir(filePath, (err) => {
+            if (err) throw new Error(err.message);
+        });
+
+        for (const child of directoryInfo) {
+            const isDirectory = (await fs.lstat(path.join(filePath, child), (err) => {
+                if (err) throw new Error(err.message);
+            })).isDirectory();
+            isDirectory ?
+                console.log(`FOLDER: ${child}`) :
+                console.log(`FILE: ${child}`);
+        }
+    } catch (e) {
+        throw new Error(e.message);
+    }
+}
+
+createFiles().then(() => {}).catch(e => e.message);
+// childIsDirectoryOrFile().then(() => {}).catch(e => e.message);
+
+
--- sayHello.js
+++
@@ -1,5 +0,0 @@
-// const sayHello = () => {
-//     console.log("Hello world from sayHello.js");
-// }
-//
-// module.exports = {sayHello}
